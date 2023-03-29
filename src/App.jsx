536cc087e81ebcb75746d3face96d6d2f3efcd07
Chl0l <chloe.kim.elle@gmail.com>
2023-03-29 12:15:02 +0200

    React Basics 05

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,22 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import PokemonCard from "./components/PokemonCard.jsx"
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
 function App() {
+  
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   return (
     <div>
-      <PokemonCard name = {pokemonList[0].name} imgSrc = {pokemonList[0].imgSrc} />
+      <PokemonCard pokemon = {pokemonList[1]} />
     </div>
   );
 }
